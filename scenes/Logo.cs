e7db46b9166bceb479fa413618887daae28891ba
Xufuja <xufuja@gmail.com>
2024-04-15 22:18:15 +0200

    Made Console appear

--- scenes/Logo.cs
+++ scenes/Logo.cs
@@ -6,11 +6,12 @@ public partial class Logo : Sprite2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Console.WriteLine("ttest");
+		Console.WriteLine("Ready");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Console.WriteLine("Processing");
 	}
 }
