1372fc27a4e7004fc90a3f5f4745a7bbb92fe402
Xufuja <xufuja@gmail.com>
2024-04-15 22:32:23 +0200

    Basic script runs

--- scenes/Logo.cs
+++ scenes/Logo.cs
@@ -6,12 +6,12 @@ public partial class Logo : Sprite2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Console.WriteLine("Ready");
+		Position = new Vector2(100, 200);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Console.WriteLine("Processing");
+		Position = new Vector2(Position.X + 10, Position.Y);
 	}
 }
