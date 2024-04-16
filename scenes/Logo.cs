49d98bc05f6d5f8523a741edcfa026da03aeb54a
Xufuja <xufuja@gmail.com>
2024-04-16 21:27:12 +0200

    Added the variables testing

--- scenes/Logo.cs
+++ scenes/Logo.cs
@@ -3,15 +3,28 @@ using System;
 
 public partial class Logo : Sprite2D
 {
+	private const int speed = 10;
+	private Vector2 position = Vector2.Zero;
+	private int testScale = 1;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Position = new Vector2(100, 200);
+		position = new Vector2(300, 200);
+		testScale = 2;
+		Position = position;
+		Scale = new Vector2(testScale, testScale);
+
+		int testRotation = 45;
+		RotationDegrees = testRotation;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Position = new Vector2(Position.X + 10, Position.Y);
+		position = new Vector2(position.X + speed, position.Y);
+		Position = position;
+		testScale += 1;
+		Scale = new Vector2(testScale, testScale);
 	}
 }
