eb37e566816dbbb8cc821682e38731e046c8895b
Xufuja <xufuja@gmail.com>
2024-04-19 22:08:35 +0200

    Added Player collision

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -12,7 +12,8 @@ public partial class Player : Node2D
 	public override void _Process(double delta)
 	{
 		Vector2 direction = Input.GetVector("left", "right", "up", "down");
-		Position += new Vector2((float)(direction.X * 500 * delta), (float)(direction.Y * 500 * delta));
+		Velocity = new Vector2(direction.X * 500, direction.Y * 500);
+		MoveAndSlide();
 
 		if (Input.IsActionPressed("primary action"))
 		{
