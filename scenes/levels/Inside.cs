e2cb718d5d49d92650b4f2bfc6e7957b03479d14
Xufuja <xufuja@gmail.com>
2024-05-15 22:40:44 +0200

    Added Inside level

---
+++ scenes/levels/Inside.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Inside : Level
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnExitGateAreaBodyEntered(PhysicsBody2D _body)
+	{
+        Tween tween = CreateTween();
+        tween.TweenProperty(GetNode("Player"), "speed", 0, 0.5f);
+    }
+}
