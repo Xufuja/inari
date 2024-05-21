1e2c861b1e87d585e3188bee953bfbe2c02492b8
Xufuja <xufuja@gmail.com>
2024-05-21 21:29:29 +0200

    Updating UI colors based on ammo

--- scenes/user_interface/UI.cs
+++ scenes/user_interface/UI.cs
@@ -5,12 +5,19 @@ public partial class UI : CanvasLayer
 {
 	private Label laserLabel;
     private Label grenadeLabel;
+    private TextureRect laserIcon;
+    private TextureRect grenadeIcon;
+
+    private Color green = new Color("6BBFA3");
+    private Color red = new Color(0.9f, 0, 0, 1);
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
         laserLabel = GetNode<Label>("LaserCounter/VBoxContainer/Label");
         grenadeLabel = GetNode<Label>("GrenadeCounter/VBoxContainer/Label");
+        laserIcon = GetNode<TextureRect>("LaserCounter/VBoxContainer/TextureRect");
+        grenadeIcon = GetNode<TextureRect>("GrenadeCounter/VBoxContainer/TextureRect");
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,11 +29,26 @@ public partial class UI : CanvasLayer
 
 	public void UpdateLaserText()
 	{
-		laserLabel.Text = Globals.laserAmount.ToString(); 
+		laserLabel.Text = Globals.laserAmount.ToString();
+        UpdateColor(Globals.laserAmount, laserIcon, laserLabel);
 	}
 
     public void UpdateGrenadeText()
     {
         grenadeLabel.Text = Globals.grenadeAmount.ToString();
+        UpdateColor(Globals.grenadeAmount, grenadeIcon, grenadeLabel);
+    }
+
+    public void UpdateColor(int amount, TextureRect icon, Label label)
+    {
+        if (amount <= 0)
+        {
+            label.Modulate = green;
+            icon.Modulate = green;
+        } else
+        {
+            label.Modulate = red;
+            icon.Modulate = red;
+        }
     }
 }
