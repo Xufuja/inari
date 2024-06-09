466ba4bf7a7f66c28780518ef97905e032fe2ae3
Xufuja <xufuja@gmail.com>
2024-06-09 21:29:43 +0200

    Finished Bug enemy

--- scenes/enemies/Bug.cs
+++ scenes/enemies/Bug.cs
@@ -3,79 +3,83 @@ using System;
 
 public partial class Bug : CharacterBody2D
 {
-	private bool active = false;
-	private int speed = 300;
-	private bool vulnerable = true;
-	private bool playerNear = false;
-	private int health = 20;
+    private bool active = false;
+    private int speed = 300;
+    private bool vulnerable = true;
+    private bool playerNear = false;
+    private int health = 20;
 
-	public override void _Process(double delta)
-	{
-		Vector2 playerPosition = GetNode<Globals>("/root/Globals").PlayerPosition;
+    public override void _Process(double delta)
+    {
+        Vector2 playerPosition = GetNode<Globals>("/root/Globals").PlayerPosition;
         Vector2 direction = (playerPosition - Position).Normalized();
-		Velocity = new Vector2(direction.X * speed, direction.Y * speed);
+        Velocity = new Vector2(direction.X * speed, direction.Y * speed);
 
-		if (active)
-		{
-			MoveAndSlide();
-			LookAt(playerPosition);
-		}
-    }
-
-	public void OnAttackAreaBodyEntered(PhysicsBody2D body)
-	{
-		playerNear = true;
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("Attack");
-    }
-
-	public void OnAttackAreaBodyExited(PhysicsBody2D body)
-	{
-		playerNear = false;
-	}
-
-	public void OnNoticeAreaBodyEntered(PhysicsBody2D body)
-	{
-		active = true;
-		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("Walk");
-	}
-
-	public void OnNoticeAreaBodyExited(PhysicsBody2D body)
-	{
-		active = false;
-		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Stop();
-    }
-
-	public void OnAnimatedSprite2DAnimationFinished()
-	{
-		if (playerNear)
-		{
-			GetNode<Globals>("/root/Globals").Health -= 10;
-			GetNode<Timer>("Timers/AttackTimer").Start();
+        if (active)
+        {
+            MoveAndSlide();
+            LookAt(playerPosition);
         }
-	}
+    }
 
-	public void OnAttackTimerTimeout()
-	{
+    public void OnAttackAreaBodyEntered(PhysicsBody2D body)
+    {
+        playerNear = true;
         GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("Attack");
     }
 
-	public void OnHitTimerTimeout()
-	{
-		vulnerable = true;
-	}
+    public void OnAttackAreaBodyExited(PhysicsBody2D body)
+    {
+        playerNear = false;
+    }
 
-	public void Hit()
-	{
-		if (vulnerable)
-		{
-			vulnerable = false;
+    public void OnNoticeAreaBodyEntered(PhysicsBody2D body)
+    {
+        active = true;
+        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("Walk");
+    }
+
+    public void OnNoticeAreaBodyExited(PhysicsBody2D body)
+    {
+        active = false;
+        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Stop();
+    }
+
+    public void OnAnimatedSprite2DAnimationFinished()
+    {
+        if (playerNear)
+        {
+            GetNode<Globals>("/root/Globals").Health -= 10;
+            GetNode<Timer>("Timers/AttackTimer").Start();
+        }
+    }
+
+    public void OnAttackTimerTimeout()
+    {
+        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("Attack");
+    }
+
+    public void OnHitTimerTimeout()
+    {
+        vulnerable = true;
+        ((ShaderMaterial)GetNode<AnimatedSprite2D>("AnimatedSprite2D").Material).SetShaderParameter("progress", 0);
+    }
+
+    public async void Hit()
+    {
+        if (vulnerable)
+        {
+            vulnerable = false;
             GetNode<Timer>("Timers/HitTimer").Start();
-			health -= 10;
+            health -= 10;
+            ((ShaderMaterial)GetNode<AnimatedSprite2D>("AnimatedSprite2D").Material).SetShaderParameter("progress", 0.9f);
+            GetNode<GpuParticles2D>("Particles/HitParticles").Emitting = true;
         }
 
-		if (health <= 0)
-		{
-			QueueFree();
-		}
-	}
+        if (health <= 0)
+        {
+            await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
+            QueueFree();
+        }
+    }
 }
