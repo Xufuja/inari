3892c764d9cd14ead2ddfbe78d934cf16e7c392e
Xufuja <xufuja@gmail.com>
2024-06-10 21:49:19 +0200

    Drone follows player and can be damaged

--- scenes/enemies/Drone.cs
+++ scenes/enemies/Drone.cs
@@ -3,15 +3,47 @@ using System;
 
 public partial class Drone : CharacterBody2D
 {
+    private bool active = false;
+    private int speed = 400;
+    private bool vulnerable = true;
+    private int health = 50;
+
     public override void _Process(double _delta)
     {
-        Vector2 direction = Vector2.Right;
-        Velocity = new Vector2(direction.X * 100, direction.Y);
-        MoveAndSlide();
+        Vector2 playerPosition = GetNode<Globals>("/root/Globals").PlayerPosition;
+
+        if (active)
+        {
+            LookAt(playerPosition);
+            Vector2 direction = (playerPosition - Position).Normalized();
+            Velocity = new Vector2(direction.X * speed, direction.Y * speed);
+            MoveAndSlide();
+        }
     }
 
-    public void Hit()
+    public async void Hit()
     {
-        Console.WriteLine("Damage");
+        if (vulnerable)
+        {
+            vulnerable = false;
+            GetNode<Timer>("HitTimer").Start();
+            health -= 10;
+        }
+
+        if (health <= 0)
+        {
+            await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
+            QueueFree();
+        }
+    }
+
+    public void OnNoticeAreaBodyEntered(PhysicsBody2D body)
+    {
+        active = true;
+    }
+
+    public void OnHitTimerTimeout()
+    {
+        vulnerable = true;
     }
 }
