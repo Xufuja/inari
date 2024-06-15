c4d2aea462ab76e101d3eb29e6f9ef332c9488f6
Xufuja <xufuja@gmail.com>
2024-06-15 21:55:52 +0200

    Added lasers to car

--- scenes/enemies/Car.cs
+++ scenes/enemies/Car.cs
@@ -4,11 +4,16 @@ using System;
 public partial class Car : PathFollow2D
 {
 	private bool playerNear = false;
+	private Line2D line1;
+	private Line2D line2;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+		line1 = GetNode<Line2D>("Turret/RayCast2D/Line2D");
+        line2 = GetNode<Line2D>("Turret/RayCast2D2/Line2D");
+
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -25,10 +30,17 @@ public partial class Car : PathFollow2D
 	public void OnNoticeAreaBodyEntered(PhysicsBody2D body)
 	{
 		playerNear = true;
+		GetNode<AnimationPlayer>("AnimationPlayer").Play("LaserLoad");
 	}
 
 	public void OnNoticeAreaBodyExited(PhysicsBody2D body)
 	{
 		playerNear = false;
 	}
+
+	public void Fire()
+	{
+		GetNode<Globals>("/root/Globals").Health -= 20;
+
+    }
 }
